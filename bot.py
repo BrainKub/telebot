9a33359f2264757202e2b36f4fdf5ad08049868a
BrainKub <fyhtjnt@gmail.com>
2021-01-17 01:54:07 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import playground
 import tproger
 import covid19
+import vgtimes
 
 HEADERS = {
 		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'
@@ -12,7 +13,7 @@ HEADERS = {
 
 bot = telebot.TeleBot('1416836323:AAE6msCm5oT6ty09aHZfQwrQnWBSytqIDnY');
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Новости','Статистика COVID19')
+keyboard1.row('Новости','Статистика COVID19',)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, чем я могу тебе помочь?', reply_markup=keyboard1)
@@ -24,12 +25,15 @@ def get_text_messages(message):
         text2=tproger.get_post()
         bot.send_photo(message.chat.id, photo, caption=text, parse_mode="Markdown")
         bot.send_message(message.chat.id, text2, parse_mode="Markdown")
+
         #bot.send_message(message.chat.id, text, parse_mode="Markdown", disable_web_page_preview=False)
     elif message.text == "Статистика COVID19":
         text=covid19.get_stat()
         #bot.send_message(message.chat.id, "В разработке 0_0")
         #bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIByV_o6Z71P1Z1qrjtMtNkMz4YYDtmAAK5IgAC6VUFGKhTf2tl6fwtHgQ')
         bot.send_message(message.chat.id, text, parse_mode="Markdown", disable_web_page_preview=True)
+    elif message.text == "Халява":
+        vgtimes.get_freebie(bot, message)
     else:
         msg = message.text
         bot.send_message(message.from_user.id, "Я тебя не понимаю 0_о")
---
+++ vgtimes.py
@@ -0,0 +1,15 @@
+import telebot
+import requests
+from bs4 import BeautifulSoup
+
+def get_freebie(bot, message):
+    url = "https://vgtimes.ru/tags/%D1%85%D0%B0%D0%BB%D1%8F%D0%B2%D0%B0/"
+    response=requests.get(url)
+    html=response.text
+    soup=BeautifulSoup(html, "html.parser")
+    #test=get_tags(soup)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    texts = soup.findAll('a', '{favoritesnew}')
+    for i in range(len(texts[:-16]), -1, -1):
+        txt = str(i + 1) + ') ' + texts[i].text
+        bot.send_message(message.chat.id, '<a href="{}">{}</a>'.format(texts[i]['href'], txt), parse_mode = 'html')
